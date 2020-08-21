c9b81118686ef9ec4d07b33bbac052330a258128
Soraida Henne <sodapoph@gmail.com>
2020-08-21 01:09:47 -0500

    first commit

---
+++ TodoItem.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+function TodoItem() {
+    return (
+        <div>
+            <input type="checkbox" className="box" />
+                <label>Placeholder text here</label>
+            <br></br>
+            <input type="checkbox" className="box"/>
+                <label>Placeholder text here</label>
+            <br></br>
+            <input type="checkbox" className="box"/>
+                <label>Placeholder text here</label>
+            <br></br>
+            <input type="checkbox" className="box"/>
+                <label>Placeholder text here</label>
+        </div>
+    )
+}
+
+export default TodoItem
